63243a6b670f9be192b406e6c3c55889109d1858
ArshiaMoradi2003 <arshiamoradi1203@gmail.com>
2025-02-09 13:50:28 -0500

    add Check if any friend has total workout minutes >=120

--- assignment1_101490287.py
+++ assignment1_101490287.py
@@ -7,21 +7,30 @@ gym_member = "Alex Alliton"
 preferred_weight_kg = 20.5
 highest_reps = 25
 membership_active = True
+
 # Dictionary:
 workout_stats = {
     "Alex":(30,45,20),
     "Jamie":(25,50,15),
     "Taylor":(40,60,30)
 }
+
 # Loop through dictionary and calculate total workout minutes for each friend
 for friend in list(workout_stats.keys()):
     total_minutes = sum(workout_stats[friend])
     workout_stats[f"{friend}_total"] = total_minutes
+
 # Create 2D list (nested list) from dictionary values
 workout_list = [list(minutes) for minutes in workout_stats.values() if isinstance(minutes, tuple)]
+
 # Extract and print yoga and running minutes for all friends
 yoga_running_minutes = [row[:2] for row in workout_list]
 print("Yoga and Running minutes for all friends:" , yoga_running_minutes)
 # Extract and print weightlifting minutes for the last two friends
 weightlifting_minutes = [row[2] for row in workout_list[-2:]]
 print("Weightlifting minutes for the last two friends:", weightlifting_minutes)
+
+# Check if any friend has total workout minutes >=120
+for friend, total in workout_stats.items():
+    if isinstance(total, int) and total >= 120:
+        print(f"Great job staying active, {friend}!")
