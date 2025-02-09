9f6a72946455a13b61804eac07cc3bd0f98ae5c5
ArshiaMoradi2003 <arshiamoradi1203@gmail.com>
2025-02-09 13:29:16 -0500

    add Dictionary part

--- assignment1_101490287.py
+++ assignment1_101490287.py
@@ -6,3 +6,9 @@ gym_member = "Alex Alliton"
 preferred_weight_kg = 20.5
 highest_reps = 25
 membership_active = True
+# Dictionary:
+workout_stats = {
+    "Alex":(30,45,20),
+    "Jamie":(25,50,15),
+    "Taylor":(40,60,30)
+}
